716be6afe5ea249034053f4528b91106961a4ac1
amusulainen <anton.musulainen@gmail.com>
2024-05-31 12:55:55 +0300

    other

--- js/copy-img.js
+++ js/copy-img.js
@@ -1,26 +1,13 @@
-async function copyCanvasContentsToClipboard() {
-    
-    const canvas = document.getElementById("canvas");
+async function copyCanvasContentsToClipboard() {    
     const blob = await new Promise((resolve) => {
         canvas.toBlob(resolve, 'image/png');
       });
 
   const data = [new ClipboardItem({ [blob.type]: blob })];
 
-  await navigator.clipboard.write(data);
-
-    alert("Изображение скопировано")
+  await navigator.clipboard.write(data)
+  
+  alert("Изображение скопировано")
 }
 
-
-/* 
-
-const blob = await new Promise((resolve) => {
-  canvas.toBlob(resolve, 'image/png');
-});
-
-const item = new ClipboardItem({ "image/png": blob });
-navigator.clipboard.write([item]);
-*/
-
-
+    
